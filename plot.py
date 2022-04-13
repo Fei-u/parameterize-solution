5ce11302bb66e0dc57130b640aab6e19274193ad
Fei-u <1246277962@qq.com>
2022-04-14 06:37:24 +0800

    4.14

--- plot.py
+++ plot.py
@@ -2,5 +2,5 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def error_plt(error):
-    plt.plot(np.log10(error), "-b", label="10^(x)")
+    plt.plot(np.log10(error.cpu().np()), "-b", label="10^(x)")
     plt.show()

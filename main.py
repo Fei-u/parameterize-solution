43f82a18e65852c29a2d2195fe2345431e6228a6
Fei-u <1246277962@qq.com>
2022-04-13 16:35:22 +0800

    4.13

---
+++ main.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from libs import *
+from train import *
+from PDEs import Heat
+from plot import *
+
+
+# In[2]:
+
+
+from NNs import FNN
+
+layer_size = [3] + [100]*5 + [1]
+net = FNN.net(layer_size=layer_size)
+
+model = Heat.equation(net, 4, 1, 1)
+
+Train = Train(net, model, batch_size=2**8)
+Train.train(epoch=10**5, lr=0.0001)
+
+torch.save(net, 'net_model.pkl')
+
+errors = Train.get_errors()
+
+error_plt(errors)
+
+
+# In[ ]:
+
+
+from NNs import ResNN_F
+
+net = ResNN_F.net(3, ResNN_F.basic_block, 100)
+#收敛慢，震荡？？
+
+model = Heat(net, 4, 1, 1)
+
+Train = Train(net, model, batch_size=2**8)
+Train.train(epoch=10**5, lr=0.0001)
+
+torch.save(net, 'net_model.pkl')
+
+errors = Train.get_errors()
+
+error_plt(errors)
+

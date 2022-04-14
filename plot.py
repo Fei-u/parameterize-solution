a63005acdf581d93775fa7d302a768df43a0761c
Fei-u <1246277962@qq.com>
2022-04-14 16:17:08 +0800

    4.14-4

--- plot.py
+++ plot.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import torch
 
-def error_plt(errors):
+def error_plt(errors, i):
     plt.plot(np.log10(errors), "-b", label="10^(x)")
+    plt.savefig("error_{}.png".format(i))
     plt.show()
